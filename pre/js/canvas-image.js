1ee2a2764828b5d4699db2ffa7b70a644a789cfd
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-09-24 14:37:06 +0200

    Actualización de imagen

--- pre/js/canvas-image.js
+++ pre/js/canvas-image.js
@@ -10,7 +10,7 @@ function setChartCanvasImage() {
     // Create a link
     var aDownloadLink = document.createElement('a');
     // Add the name of the file to the link
-    aDownloadLink.download = 'envejecimiento_alzheimer_evolucion.png';
+    aDownloadLink.download = 'envejecimiento_centenarios_mapa.png';
     // Attach the data to the link
     aDownloadLink.href = image;
     // Get the code to click the download link
